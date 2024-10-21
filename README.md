f33b3efe6a6110d4f10a257a64e7a177e298dea8
Milan <dangi.milan46@gmail.com>
2024-10-21 13:24:29 +0545

    setup network related work like post and get api

--- README.md
+++ README.md
@@ -4,5 +4,7 @@
 ## Getting Started
 - Creating the different folder needed to achieve mvvm architecture
 - handling different exception like *Bad Request Exception , FetchDataException, UnauthorizedException, InvalidInputException*
+- Handling api response like completed, loading and error 
+- GetApi and PostApi are declared in abstract class named BaseApiService , defined in NetworkApiService class to fetch data  from server and post data in server accordingly and handling different status error like 404, 400.
 
 
---
+++ lib/data/network/network_api_services.dart
@@ -0,0 +1,56 @@
+
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:mvvm_learnig/data/app_exception.dart';
+import 'package:mvvm_learnig/data/network/base_api_services.dart';
+
+class NetworkApiServices extends BaseApiServices{
+  @override
+  Future getGetApiResponse(String url) async{
+    dynamic responseJson;
+
+    try{
+      final response = await http.get(Uri.parse(url)).timeout(Duration(seconds:10 ));
+      responseJson = returnResponse(response);
+
+    }on SocketException{
+      throw FetchDataException('No internet Connection');
+    }
+    return responseJson;
+
+  }
+
+  @override
+  Future getPostApiResponse(String url,dynamic data) async {
+
+    dynamic responseJson;
+
+    try{
+      final response = await http.post(
+          Uri.parse(url),
+        body:data
+      ).timeout(Duration(seconds: 10));
+      responseJson = returnResponse(response);
+
+    }on SocketException{
+      throw FetchDataException('No internet Connection');
+    }
+    return responseJson;
+  }
+  dynamic returnResponse (http.Response response){
+    switch(response.statusCode){
+      case 200:
+        dynamic responseJson =jsonDecode(response.body);
+        return responseJson;
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 404:
+        throw BadRequestException('Page doesn\'t found');
+
+      default:
+        throw FetchDataException('Error occurred while communication' + 'with status code ' + response.statusCode.toString());
+    }
+  }
+
+}
